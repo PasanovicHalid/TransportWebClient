c056d9b7db2dfcf05b9a6a4db04b891eb04cdc8c
Halid Pasanovic <halidpasanovic1000@gmail.com>
2023-02-19 00:50:05 +0100

    Setup proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TransportWebClient';
+
+  constructor() {
+    console.log(environment.production)
+    
+  }
 }
